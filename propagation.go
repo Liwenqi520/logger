b0360f6b313b79071278c659f826254195ae89b6
lican <lican@nongbotech.com>
2025-01-13 10:54:26 +0800

    init logger

---
+++ propagation.go
@@ -0,0 +1,20 @@
+package logger
+
+import (
+	"context"
+	"net/http"
+
+	"brewing-sass/gopkg/logger/propagation/extract"
+	"brewing-sass/gopkg/logger/propagation/inject"
+	"github.com/gin-gonic/gin"
+)
+
+// HTTPInject inject spanContext
+func HttpInject(ctx context.Context, request *http.Request) error {
+	return inject.HttpInject(ctx, request)
+}
+
+// GinMiddleware extract spanContext
+func GinMiddleware(service string) gin.HandlerFunc {
+	return extract.GinMiddleware(service)
+}
